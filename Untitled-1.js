beb1fb42050f274c0ea0dcd9fe644b88e6df43ad
prostoimr <nturuncev@icloud.com>
2024-09-09 13:35:51 +0500

    Add files via upload

---
+++ Untitled-1.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    // Здесь можно добавить код для обработки отправки формы
+    alert('Форма успешно отправлена!');
+});
